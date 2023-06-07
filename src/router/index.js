d0b71f0bba4570d4bc4cff7f8b5e69134deec06b
Lauren Grills <lauren.grills@gmail.com>
2023-06-07 15:33:23 -0500

    skeleton, navigation, and routing setup

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/about', name: 'About', component: () => import(/* webpackChunkName: "about" */ '../views/About.vue') },
+    { path: '/resume', name: 'Resume', component: () => import(/* webpackChunkName: "resume" */ '../views/Resume.vue') },
+    { path: '/portfolio', name: 'Portfolio', component: () => import(/* webpackChunkName: "portfolio" */ '../views/Portfolio.vue') },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+})
+
+export default router
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>You've reached the Portfolio page!</h1>
+</template>
+
+<style scoped>
+</style>
