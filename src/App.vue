e288bf18a771a41100381b633f88931ee4f9fcb1
Lauren Grills <lauren.grills@gmail.com>
2023-06-16 12:24:01 -0500

    Added fonts and contact page

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,42 @@
 
 <template>
     <div id="nav" class="navbar">
-      <div class="accent-text">Lauren Doss</div>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/resume">Resume</router-link>
-      <router-link to="/portfolio">Portfolio</router-link>
+      <div class="nav-group">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/portfolio">Portfolio</router-link>
+        <router-link to="/contact">Say Hello</router-link>
+      </div>
     </div>
-    <router-view></router-view>
+    <div class="container-view">
+      <router-view></router-view>
+    </div>
 </template>
 
 <style scoped>
+.nav-group {
+  display: flex;
+  flex-direction: row;
+  gap: 2em;
+  font-family: 'Ellograph Thin';
+  font-size: 1.2rem;
+  justify-content: space-between;
+  text-transform: uppercase;
+}
 
+.container-view {
+  margin: 0;
+  min-width: 320px;
+  min-height: 100vh;
+  gap: 8px;
+}
+
+.navbar a {
+  text-decoration: none;
+}
+
+img {
+  max-width: 150px;
+  max-height: 100px;
+}
 </style>
