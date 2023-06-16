2c6bbcb09574af7346424d1dd4b1f86e48ffd727
Lauren Grills <lauren.grills@gmail.com>
2023-06-16 12:42:36 -0500

    Added PDF Resume and removed vue page

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Home from '../views/Home.vue'
 const routes = [
     { path: '/', name: 'Home', component: Home },
     { path: '/about', name: 'About', component: () => import(/* webpackChunkName: "about" */ '../views/About.vue') },
-    { path: '/resume', name: 'Resume', component: () => import(/* webpackChunkName: "resume" */ '../views/Resume.vue') },
     { path: '/portfolio', name: 'Portfolio', component: () => import(/* webpackChunkName: "portfolio" */ '../views/Portfolio.vue') },
     { path: '/contact', name: 'Contact', component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue') }
 ]
